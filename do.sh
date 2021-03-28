15b05eaa5cf3e92a718a9915ae7d54e410b84575
Vadim Bauer <vb@8gears.com>
2021-03-29 01:18:08 +0200

    init

---
+++ do.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# Do - The Simplest Build Tool on Earth.
+# Documentation and examples see https://github.com/8gears/do
+
+set -e -u # -e "Automatic exit from bash shell script on error"  -u "Treat unset variables and parameters as errors"
+
+generateClient() {
+  openapi-generator generate -i harbor-openapi-3.yaml -g python --package-name harbor-client \
+  --additional-properties=packageVersion=2.2  --additional-properties=projectName=harbor-client -o .
+}
+
+
+"$@" # <- execute the task
+
+[ "$#" -gt 0 ] || printf "Usage:\n\t./do.sh %s\n" "($(compgen -A function | grep '^[^_]' | paste -sd '|' -))"
+
