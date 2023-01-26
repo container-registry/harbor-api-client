f4de3fd991ecd8fa1b3c141d1e1dfcc26f48b227
Alexandre Krispin <development.s004k@simplelogin.com>
2023-01-26 14:00:25 +0900

    Update repo name

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "harbor_client"
-VERSION = "2.7.0"
+VERSION = "2.7.4"
 # To install the library, run the following
 #
 # python setup.py install
